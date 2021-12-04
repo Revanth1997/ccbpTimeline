725bbfc866851d6cb0aa159ffab27b02c31d819e
Revanth1997 <revanth1997.rn@gamil.com>
2021-12-04 14:23:09 +0000

    timeline

---
+++ src/components/TimelineView/styledComponents.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components/macro'
+
+export const TimelineContainer = styled.div``
+
+export const ResponsiveContainer = styled.div``
+
+export const HeaderContainer = styled.div``
+
+export const Heading = styled.h1`
+  text-align: center;
+  color: #171f46;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 2.7;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+`
+
+export const CCBPHeading = styled.span`
+  color: #2b237c;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 24px;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+`
